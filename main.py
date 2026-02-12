03c833d719bb6f1b45f0bb3efc7f22244798160b
saikiaDeepAlt <saikiadeep37@gmail.com>
2026-02-12 16:52:51 +0530

    correct assignment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from calc import multiply, subtract
 assert multiply(5,5)==25
-assert multiply(6,6)==35
+assert multiply(6,6)==36
 assert subtract(6,1)==5
-assert subtract(45,5)==9
+assert subtract(45,5)==40
